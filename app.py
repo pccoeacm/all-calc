5774aebd48f78534816a522c946d0ff3df799583
suyashsonawane <suyashsonawane007@gmail.com>
2020-10-03 00:38:02 +0530

    new functions added

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ class Calc:
         self.functions = {}
         self.options = []
         self.load()
-        self.build_options()
 
     def load(self):
         modules = [f.split('\\')[1] for f in glob.glob("functions/*.py")]
@@ -19,18 +18,41 @@ class Calc:
                 f'functions.{mods.split(".")[0]}')
 
         for name, func in self.operators.items():
-            self.functions[name] = inspect.getmembers(func,
-                                                      inspect.isfunction)[0][1]
+
+            fns = inspect.getmembers(func, inspect.isfunction)
+
+            if (len(fns) == 0):
+                print(f"No functions in {name}.py")
+                exit()
+
+            found = False
+            for f in fns:
+                if f[0] == 'calc':
+                    self.functions[name] = f[1]
+                    found = True
+
+            if not found:
+                print(f"Can't find calc function in {name}.py")
+
+            # self.functions[name] = inspect.getmembers(func,
+            #                                           inspect.isfunction)[0][1]
             self.options.append(name)
 
     def build_options(self):
+        print("\n")
         for i, option in enumerate(self.options):
             print(i + 1, option)
 
-        ui = int(input("Which opertaions to excute > "))
+        ui = int(input("\nWhich operations to excute > "))
 
-        self.functions[self.options[ui - 1]]()
+        if (ui > len(self.options or ui < 0)):
+            print("Incorrect Option")
+
+        else:
+            self.functions[self.options[ui - 1]]()
 
 
 c = Calc()
-c.build_options()
+
+while True:
+    c.build_options()
--- functions/div.py
+++ functions/div.py
@@ -1,7 +1,6 @@
 def calc():
-    
+
     a = int(input("Enter a >>"))
     b = int(input("Enter b >>"))
 
-
-    print(f"Solution {a+b}")
+    print(f"Solution {a/b}")
