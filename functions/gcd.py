cf3af162ad8f67d1e6e6de65322d8688930277fe
MEHUL25 <mehull2412@gmail.com>
2020-10-03 16:47:16 +0530

    Success

---
+++ functions/gcd.py
@@ -0,0 +1,28 @@
+def calc(should_print=False):
+
+    print("""
+    Name: GCD(Greatest Common Divisor) using Euclid's Algorithm
+    Operation : GCD of 2 numbers 
+    Inputs : a->int , b->int
+    Outputs: c=>gcd(a,b) ->int
+    Author : MEHUL25  
+    \n
+    """)
+
+    a = int(input("Enter a >>"))
+    b = int(input("Enter b >>"))
+
+    result = {}
+    result['inputs'] = [a, b]
+
+    while(b): 
+       a, b = b, a % b 
+
+    result['outputs'] = [a]
+
+    if should_print:
+        print(f"Solution {result['outputs'][0]}")
+    else:
+        return result
+
+calc(True)
