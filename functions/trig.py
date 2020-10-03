210204c4568b194cc1d16b92d43cfd32babd1ec6
unnatibhalekar <unnatibhalekar12345@gmail.com>
2020-10-03 16:09:24 +0530

    add sine function

---
+++ functions/trig.py
@@ -0,0 +1,23 @@
+import math
+def calc(should_print=False):
+    
+    print("""
+    Name: trignometric sine value
+    Operation : finding the trignometric value of the given input
+    Inputs : a->int/float
+    Outputs: c=>sin(a) ->float
+    Author : UnnatiBhalekar
+    \n
+    """)
+
+    a = float(input("Enter a >>"))
+    c = round(math.sin(a))
+    result = {}
+    result['inputs'] = [a]
+    result['outputs'] = [c]
+
+    if should_print:
+        print(f"Solution {result['outputs'][0]}")
+    else:
+        return result
+
