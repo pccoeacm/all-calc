f400468a0356303179f3a36deea1903ade0d1d84
anirudh <geetha021970@gmail.com>
2020-10-03 19:49:26 +0530

    Added Exponentiation

---
+++ functions/exponentiation.py
@@ -0,0 +1,22 @@
+def calc(should_print=False):
+
+    print("""
+    Name: Exponentiation
+    Operation : Take base to the power of exponent 
+    Inputs : a->int/float , b->int/float
+    Outputs: c=>a^b ->float
+    Author : Anirudh Prabhakaran  
+    \n
+    """)
+
+    a = float(input("Enter a (base) >>"))
+    b = float(input("Enter b (exponent) >>"))
+
+    result = {}
+    result['inputs'] = [a, b]
+    result['outputs'] = [a ** b]
+
+    if should_print:
+        print(f"Solution {result['outputs'][0]}")
+    else:
+        return result
