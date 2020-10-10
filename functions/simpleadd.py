2e204b9c2d3e8ab63ea561eeedbc6195f8616b79
axtant <70051483+axtant@users.noreply.github.com>
2020-10-10 09:49:31 +0530

    Create simpleadd.py

---
+++ functions/simpleadd.py
@@ -0,0 +1,9 @@
+# Store input numbers
+numbr1 = input("Enter first number= ")
+numbr2 = input("Enter second number= ")
+
+# code
+sum = float(num1) + float(num2)
+
+# resuly
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
